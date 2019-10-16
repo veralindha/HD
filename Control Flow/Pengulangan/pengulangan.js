05941d4748312a641d9bdef82c9613741b112d54
veralindha <30642445+veralindha@users.noreply.github.com>
2019-10-16 10:13:28 +0700

    Add files via upload

---
+++ Control Flow/Pengulangan/pengulangan.js
@@ -0,0 +1,45 @@
+/*
+
+
+
+untuk pengulangan javascript dibagi menjadi 3 yaitu
+for, while, do while
+- rumus while
+  masukan nilai
+  proses
+  increment dan decrement
+- rumus for
+ masukan nilai
+ proses
+- rumus do while
+
+
+/*
+var i = true;
+while(i)
+{
+i = prompt('masukan nama :');
+alert('hai ' + i)
+i = confirm('lagi');
+}
+alert('terimakasih');
+*/
+
+// var p = 1;
+// while( p <=3 )
+// {
+// alert('lakukan yang ini');
+// p++
+// }
+
+// var o = true;
+// while( o <3){
+//     alert('yang ini');
+//     o++;
+// }
+
+alert('mulai');
+for(var u = 1; u < 2; u++){
+alert('hallo');
+}
+alert('terimakasih');
---
+++ Control Flow/pengkondisian/pengkondisian.js
@@ -0,0 +1,13 @@
+// // var angka = prompt('masukan nama :');
+// // if(angka % 2 === 0)
+// // {
+// //     alert( angka + ' adalah bilangan GENAP');
+// // }else {
+// //     alert( angka + ' adalah bilangan GANJIL');
+// // }
+// console.log('hai');
+var u = true;
+while(u)
+{
+    console.log('hai');
+}

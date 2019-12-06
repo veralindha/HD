a046450d953d019ab408e68638e81602722fc49a
veralindha <30642445+veralindha@users.noreply.github.com>
2019-12-06 10:48:41 +0700

    Add files via upload

---
+++ Control Flow/loop.js
@@ -0,0 +1,21 @@
+function loop() {
+    //pengulangan dengan do while memang spesial wkwlwkwkwk
+    //     let f: number;
+    //     f = 50;
+    //     do{
+    //         f = f - 3;
+    //         console.log("pengulangan ke : " + f);
+    //     }
+    //     while(f>0 );
+    // //pengulangan menggunakan for
+    // for(let f = 50; f>=0; f=f-2){
+    //     console.log("loop for ke :" +f);
+    // }
+    // for(let f = 1; f <= 10; f++){
+    //     for(let k = 1; k <= f; k++)
+    //     console.log("pengulangan for ke :" + f + "|" + k)
+    // }
+    for (var f = "*"; f <= "*****"; f = f + "*") {
+        console.log(f);
+    }
+}
